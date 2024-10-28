494966900ce60c5fb7d3ccfc1840eb1722c5d78c
rlawjdgkr <irlawjdgkr@naver.com>
2024-10-28 11:24:09 +0900

    js-1-2변수 학습

---
+++ 1-2-변수/1-variable.js
@@ -0,0 +1,34 @@
+var num = 10 + 20;    //저장
+console.log(num * 2);  //출력
+
+var doubleNum = num * 2;  //60 저장
+console.log(doubleNum * 2); //120 출력
+
+
+num = 10 // 값 10으로 변경
+console.log(num *2 );       //초기 변수 선언시에는 var 사용 그 이후에는 var사용 안해두 댐
+
+//변수 선언 (생성)
+// var 7friendName;    //변수 선언시 첫글자에 숫자 and 숫자만으로 만드는거는 안됨
+
+// var friend name;  // 이름을 지을  때 띄어쓰기 불가능
+
+var my_friend_name;  //snake case : SQL. python 
+var myFriendName; // camel case : Java, Javascript
+//특수문자는 $ , _ 만 사용가능
+var $friendName_;
+
+//예약어(키워드)는 이름지정 불가능
+// var for;
+
+var userName;
+console.log(userName);
+
+//변수의 초기화(initialized)
+userName = '김철수';
+console.log(userName);
+
+//변수를 선언하면서 초기화
+var address = '서울시';
+address ='경기도'; // 변수값 수정( 수정시에는 var를 안붙임)
+
