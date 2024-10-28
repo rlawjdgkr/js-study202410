98ae6c0277425a12dfbf86d3466ad0980e31f06c
rlawjdgkr <irlawjdgkr@naver.com>
2024-10-28 11:24:31 +0900

    js-1-3-데이터타입학습

---
+++ 1-3-데이터타입/1-basic-type.js
@@ -0,0 +1,12 @@
+
+let age = 20;
+console.log(typeof age);
+
+
+let taxRate = 0.15;
+console.log(typeof taxRate);
+
+console.log(age / 0); // Infinity
+console.log(age * '메롱');  //NaN : Not an Number
+
+
