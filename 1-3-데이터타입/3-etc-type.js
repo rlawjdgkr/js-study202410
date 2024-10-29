0211adf1c6cb7f758a6d6d33ed5214bae08bd60b
rlawjdgkr <irlawjdgkr@naver.com>
2024-10-29 09:35:41 +0900

    ㅁㄴㅇ

---
+++ 1-3-데이터타입/3-etc-type.js
@@ -0,0 +1,17 @@
+
+//논리 타입(boolean)
+let flag = false;
+console.log(typeof flag);
+
+let num = 3;
+let result = num == 5 ;  //소문자 true false 만 인식, 따음표도 안됨
+console.log(result);
+
+// null 타입 - 부존재 (명시적인 없음)
+let props = 'null';
+console.log(typeof props);
+
+// undefined 타입 - 변수가 초기화되지 않는 상태
+let yourName ;
+console.log(yourName);
+
