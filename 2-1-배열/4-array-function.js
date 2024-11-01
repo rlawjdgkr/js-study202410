aaccd2fb87c52243a2ac480362ff64d9947560a1
rlawjdgkr <irlawjdgkr@naver.com>
2024-11-01 15:43:53 +0900

    11-01 오후시간

--- 2-1-배열/4-array-function.js
+++ 2-1-배열/4-array-function.js
@@ -16,7 +16,7 @@ let flag = foodList.includes('파스타');
 console.log(flag);
 
 if (foodList.includes('파스타')){
-  foodList.push('hi');
+
 }
 console.log(foodList);
 
@@ -27,6 +27,7 @@ let number = [0 , 1 , 2 , 3 , 4 , 5 , 6];
 let slicedNumbers = number.slice(1, 3);
 console.log('slicedNumbers', slicedNumbers);
 
+//slice() : 배열을 우너하는 만큼 분할해서 복사배열로 리턴
 
 let copy1 = number.slice(3) //end 생략시 끝까지 분할
 console.log(copy1);
@@ -35,6 +36,12 @@ let copy2 = number.slice(); //둘다 생략시 처음부터 끝까지 복제
 console.log(copy2);
  
 
-//slice() : 배열을 우너하는 만큼 분할해서 복사배열로 리턴
+//splice() : 배열의 요소를 추가하거나 삭제 (중간에서 작업 가능)
+// 배열 원본이 손상됨  - 안전한 작업을 위해서는 복사하고서 작업
+console.log('============================');
+console.log(foodList);
+foodList.splice(1,1, '마라탕')
+console.log(foodList);
 
-
+foodList.splice(2, 0 ,'짜장면')
+console.log(foodList);
---
+++ 2-1-배열/5-quiz2.js
@@ -0,0 +1,11 @@
+let arrayList = ['유노윤호','최강창민','영웅재중','믹키유천','시아준수']
+
+let min = arrayList.indexOf('최강창민');
+
+console.log(min);
+if(arraylist === '최강창민'){
+arrayList.slice(min)
+console.log(arrayList);
+}
+
+
